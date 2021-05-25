41bc5d9587c1345862026c6d8ba638d488c2ed55
Jacob Hubbard <hubbarj6@miamioh.edu>
2021-05-25 03:46:56 -0400

    crud operations on link schema

--- src/index.js
+++ src/index.js
@@ -1,57 +1,79 @@
 const { ApolloServer } = require('apollo-server');
-
-
-
-/*
-The code below defines the GraphQL schema
-
-Simple *query* type with one field called *info*
-Type: "String!" means required and never can be null
-*/
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-      id: ID!
-      description: String!
-      url: String!
-  }
-`
-
-
+const fs = require('fs');
+const path = require('path');
 
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
+},
+{
+    id: 'link-1',
+    url: 'www.twitch.tv',
+    description: 'twitch url for no reason'
 }]
 
 /*
 The actual implementation of the GraphQL schema.  
 Structure is identical to the structure of the type definition inside typeDefs: Query.info.
 */
+let idCount = links.length
 const resolvers = {
     Query: {
         info: () => 'This is a test API for hackernews clone',
         feed: () => links,
+        link: (parent, args) =>
+            links.find(link => link.id == args.id)
     },
 
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+        },
+
+        updateLink: (parent, args) => {
+            let linkToUpdate = {}
+            var newLinks = links.map(link => {
+                if(link.id == args.id) {
+                    linkToUpdate = Object.assign(
+                        {}, 
+                        link, 
+                        {id: link.id, url: args.url,description: args.description}
+                     )
+                    return linkToUpdate
+                }
+                return link
+            });
+            links = newLinks
+
+            return linkToUpdate
+        },
+
+        deleteLink: (parent, args) => {
+            const linkToRem = links.find(link => link.id == args.id)
+            links = links.filter(link => link.id != args.id)
+            return linkToRem
+        }
     }
 }
+
+
 
 
 /*
 This tells the server what API operations are accepted and how they should be resolved
 */
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'), 
+        'utf8'
+    ),
     resolvers,
 })
 
